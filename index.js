5443ab9e8c59f33d1d701c8f3d16e36326c1b8dc
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-07-21 08:37:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function gree(hours) {
+    let time = hours.toInt
+    if(hours < 12 ) {
+      return 'Goood Morning';
+    } else if(hours >= 12 && hours <= 17) {
+      return 'Good Afternooon';
+    } else {
+      return 'Good Evening';
+    }
+}
+
 /* Write your implementation of displayMessage() */
+
+
+
+
